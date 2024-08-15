f085d6a1398e735c6216b97625a6ccf09e5c10b3
Nandan Singh Khani <91587190+NandanSinghKhani@users.noreply.github.com>
2024-08-15 21:23:53 +0530

    Add files via upload
    python

---
+++ Python/Chapter7-PS/05_problem5.py
@@ -0,0 +1,9 @@
+# Write a program to calculate the factorial of a given number using for loop.
+n = int(input("Enter the number: "))
+i = 1
+sum = 0
+while(i<=n):
+    sum += i
+    i+=1
+
+print(sum)
